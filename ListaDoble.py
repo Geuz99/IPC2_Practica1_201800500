96c7f9228c7529fe1c052bd60abb8a6485b2aff4
unknown <gerardo.zuleta15@gmail.com>
2021-06-06 17:29:28 -0600

    first commit

---
+++ ListaDoble.py
@@ -0,0 +1,99 @@
+class Nodo(object):
+    def __init__(self, nombre=None, apellido=None, telefono=None, siguiente=None, anterior=None):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.siguiente = siguiente
+        self.anterior = anterior
+
+
+class ListaDoblementeEnlazada(object):
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+        self.contador = 0
+
+    def insertar(self, nombre, apellido, telefono):
+        nodo = Nodo(nombre, apellido, telefono)
+
+        if self.cabeza is None:
+            self.cabeza = nodo
+            self.cola = self.cabeza
+        else:
+            nodo.anterior = self.cola
+            self.cola.siguiente = nodo
+            self.cola = nodo
+
+        self.contador += 1
+
+    def insertar_inicio(self, nombre, apellido, telefono):
+        if self.cabeza is not None:
+            nodo = Nodo(nombre, apellido, telefono)
+            nodo.siguiente = self.cabeza
+            self.cabeza.anterior = nodo
+            self.cabeza = nodo
+
+            self.contador += 1
+
+    def buscar_telefono(self, telefono):
+        for d in self.iterar():
+            if telefono == d:
+                return True
+        return False
+
+    def dame_telefono(self, telefono):
+        if self.cabeza is None:
+            print("List has no element")
+            return
+        else:
+            n = self.cabeza
+            while n is not None:
+                if telefono == n.telefono:
+                    return True
+                else:
+                    n = n.siguiente
+
+    def dame_nombre(self, telefono):
+            n = self.cabeza
+            while n is not None:
+                if telefono == n.telefono:
+                    return n.nombre
+                else:
+                    n = n.siguiente
+
+    def dame_apellido(self, telefono):
+            n = self.cabeza
+            while n is not None:
+                if telefono == n.telefono:
+                    return n.apellido
+                else:
+                    n = n.siguiente
+
+    def validacion_telefono(self, telefono):
+        n = self.cabeza
+        while n is not None:
+            if telefono == n.telefono:
+                return telefono
+            else:
+                n = n.siguiente
+
+    def iterar(self):
+        actual = self.cabeza
+        while actual:
+            nombre = actual.nombre
+            apellido = actual.apellido
+            telefono = actual.telefono
+            actual = actual.siguiente
+            yield nombre, apellido, telefono
+
+    def recorrer_lista(self):
+        if self.cabeza is None:
+            print("List has no element")
+            return
+        else:
+            n = self.cabeza
+            while n is not None:
+                print(n.nombre, " ")
+                print(n.apellido, " ")
+                print(n.telefono, " ")
+                n = n.siguiente
---
+++ main.py
@@ -0,0 +1,64 @@
+from ListaDoble import *
+
+datos = ListaDoblementeEnlazada()
+
+
+def menu_agenda():  # MENU DE OPCIONES
+    print('--------------MENU--------------')
+    print('1.INGRESAR NUEVO CONTACTO.')
+    print('2.BUSCAR CONTACTO.')
+    print('3.VISUALIZAR AGENTA.')
+
+
+def start_menu():
+    opcion = -1
+
+    while opcion != 4:
+        menu_agenda()
+
+        try:
+            opcion = int(input('Elija una opción >>>>>>>>>>>> '))
+        except Exception:
+            print('opción no disponible')
+
+        if opcion == 1:
+            nombre = input("Ingrese nombre: \n")
+            apellido = input("Ingrese apellido: \n")
+            telefono = input("Ingrese numero de telefono: \n")
+
+            if telefono == datos.validacion_telefono(telefono):
+                print('El contacto ya existe.')
+            else:
+                datos.insertar(nombre, apellido, telefono)
+                print("El contacto se ha agregado exitosamente")
+                # print(datos.contador)
+
+        if opcion == 2:
+            numero = input('Ingrese el numero por buscar: \n')
+            if datos.dame_telefono(numero):
+                print('encontrado')
+                print("Nombre: ", datos.dame_nombre(numero) + "\nApellido: ", datos.dame_apellido(numero) +
+                      "\nNumero de telefono: ", numero)
+            else:
+                #print("no encontrado")
+                print("El numero de telefono no existe.")
+                agregar = input("Desea agregarlo? Si o No _")
+
+                if agregar == "si":
+                    nombre = input("Ingrese nombre: \n")
+                    apellido = input("Ingrese apellido: \n")
+                    telefono = input("Ingrese numero de telefono: \n")
+                    if telefono == datos.validacion_telefono(telefono):
+                        print('El contacto ya existe.')
+                    else:
+                        datos.insertar(nombre, apellido, telefono)
+                        print("El contacto se ha agregado exitosamente")
+
+        if opcion == 3:
+            datos.recorrer_lista()
+
+            # for i in datos.iterar():
+            #   print(i)
+
+
+start_menu()
